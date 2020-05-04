0df755cfbb19851a2cf534d2076883252457a9b9
halvorboe <halvor@highered.global>
2020-05-04 10:02:41 +0200

    Did some formatting

--- up/app.py
+++ up/app.py
@@ -1,15 +1,10 @@
-from sanic import Sanic
-from sanic.response import json, file
 import os
+import subprocess
 import time
 import uuid
-import subprocess
 
-
-def convert(docx, markdown):
-    subprocess.call(f"pandoc -s {docx} -t markdown -o {markdown}".split())
-
-
+from sanic import Sanic
+from sanic.response import file, json
 
 TEMP = "temp"
 UPLOADS = "uploads"
@@ -17,13 +12,18 @@ UPLOADS = "uploads"
 
 app = Sanic(__name__)
 
+
+def convert(docx, markdown):
+    subprocess.call(f"pandoc -s {docx} -t markdown -o {markdown}".split())
+
+
 @app.route("/", methods=["POST", "GET"])
 async def index(request):
     return await file("index.html")
 
-@app.route("/upload", methods=['POST'])
-async def upload(request):
 
+@app.route("/upload", methods=["POST"])
+async def upload(request):
 
     post_file = request.files.get("file")
 
@@ -33,7 +33,7 @@ async def upload(request):
 
     if not name.endswith(".docx"):
         return json({"error": "file needs to be docx"})
-    
+
     doc_id = uuid.uuid4()
 
     temp_path = f"{TEMP}/{doc_id}.docx"
@@ -44,20 +44,16 @@ async def upload(request):
 
     convert(temp_path, upload_path)
 
-
-
-
-
     return await file("happy.html")
+
 
 def make_dir(name):
     if not os.path.exists(name):
         os.makedirs(name)
 
+
 if __name__ == "__main__":
     make_dir(TEMP)
     make_dir(UPLOADS)
-
-
 
     app.run(host="0.0.0.0", port=8080, debug=True)
