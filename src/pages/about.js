1cb289ead794bc50ae7ca118c833f80d5953c56c
halvorboe <halvor@highered.global>
2020-05-11 09:00:22 +0200

    Added basics of about

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const image = "";
+
+const About = () => {
+  return (
+    <div style={{display: "flex"}}>
+        <div style={{width: "50vw", height: "100vh"}} className="portrait">
+        </div>
+        <div  style={{width: "50vw"}}>
+            <h1>Halvor Fladsrud Bø</h1>
+            <p>test</p>
+        </div>
+    </div>
+  );
+};
+
+export default About;
