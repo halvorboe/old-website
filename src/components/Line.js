cc26095febc2cfb6651a4edf58fa42a0b0e727c2
halvorboe <halvor@highered.global>
2020-05-11 08:25:29 +0200

    refactor

---
+++ src/components/Line.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Line = ({margin}) => {
+    return (
+        <div style={{marginBottom: 40}}>
+            <hr />
+        </div>
+    )
+}
+
+export default Line;
